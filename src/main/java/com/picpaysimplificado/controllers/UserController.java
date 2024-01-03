a90f24766aba8570efe298c197fbe0d9c734f0aa
jpedrosantosb <jpedrosantosb@gmail.com>
2024-01-03 20:52:21 -0300

    Transaction controller

--- src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -6,19 +6,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dto.UserDTO;
 import com.picpaysimplificado.services.UserService;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
 public class UserController {
 
 	private UserService userService;
 	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
 	@PostMapping
-	public ResponseEntity<User> createUser(UserDTO user) {
+	public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
 		User newUser = userService.createUser(user);
 		return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 	}
